e935b05ebe559ec1b72321316037051e767e815f
Francis Conroy <francisc@posteo.net>
2025-12-02 21:52:34 +1100

    working version part II

--- day_2_py/main.py
+++ day_2_py/main.py
@@ -6,9 +6,6 @@ class ItemRange:
     start: int
     end: int
 
-fancy_items = []
-invalid_ids = []
-
 def is_item_valid(item: int) -> bool:
     itstring = str(item)
     if len(itstring)%2 == 0:
@@ -17,15 +14,35 @@ def is_item_valid(item: int) -> bool:
             return False
     return True
 
-with open("sample.txt", "r") as readfile:
-    content = readfile.read()
-    items = content.split(",")
-    for item in items:
-        start, end = item.split("-")
-        fancy_items.append(ItemRange(int(start), int(end)))
+def is_item_valid_advanced(item: int) -> bool:
+    itstring = str(item)
+    itemlengths = range(1, len(itstring)//2 + 1)
+    itemlengths_refined = [l for l in itemlengths if len(itstring)%l == 0]
+    for itemlen in itemlengths_refined:
+        itemcount = len(itstring)//itemlen
+        refitem = itstring[0:itemlen]
+        if itstring.count(refitem) == itemcount:
+            return False
 
-for item_range in fancy_items:
-    for item_id in range(item_range.start, item_range.end + 1):
-        if not is_item_valid(item_id):
-            invalid_ids.append(item_id)
-print(sum(invalid_ids))
+    return True
+
+
+if __name__ == "__main__":
+    fancy_items = []
+    invalid_ids = []
+    advanced_invalid_ids = []
+    with open("sample.txt", "r") as readfile:
+        content = readfile.read()
+        items = content.split(",")
+        for item in items:
+            start, end = item.split("-")
+            fancy_items.append(ItemRange(int(start), int(end)))
+
+    for item_range in fancy_items:
+        for item_id in range(item_range.start, item_range.end + 1):
+            if not is_item_valid(item_id):
+                invalid_ids.append(item_id)
+            if not is_item_valid_advanced(item_id):
+                advanced_invalid_ids.append(item_id)
+    print(sum(invalid_ids))
+    print(sum(advanced_invalid_ids))
