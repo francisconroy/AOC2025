d0b3defd08ae6a50d23360d0faf23a8f0806e7de
Francis Conroy <francisc@posteo.net>
2025-12-03 18:37:33 +1100

    Cracked it

--- day_3/main.py
+++ day_3/main.py
@@ -1,7 +1,23 @@
 
 
+def process_bank(digits_to_select: int, bank_array: list[int]) -> int:
+    selected_digits = []
+    selection_array = bank_array.copy()
+    for i in range(digits_to_select, 0, -1):
+        number_remaining = i-1
+        if number_remaining <=0:
+            possibles = selection_array
+        else:
+            possibles = selection_array[0:-number_remaining]
+        selected_digit = max(possibles)
+        selected_digits.append(selected_digit)
+        digpos = selection_array.index(selected_digit)
+        selection_array = selection_array[digpos + 1:]
+    comb = "".join([str(i) for i in selected_digits])
+    return int(comb)
+
 if __name__ == '__main__':
-    joltages = []
+    part_1_joltages = []
     with open("sample.txt", "r") as readfile:
         bank_strings = [l.strip() for l in readfile.readlines()]
         bank_arrays = []
@@ -9,13 +25,16 @@ if __name__ == '__main__':
             bank_array = [int(digit) for digit in bank_string]
             bank_arrays.append(bank_array)
 
-            first_digit = max(bank_array[0:-1])
-            digpos = bank_array.index(first_digit)
-            end_array = bank_array[digpos + 1 :]
-            second_digit = max(end_array)
-            comb = int(f"{first_digit}{second_digit}")
-            joltages.append(comb)
+    for bank_array in bank_arrays:
+        part_1_joltages.append(process_bank(2, bank_array))
 
-    print(joltages)
+    part_2_joltages = []
+    for bank_array in bank_arrays:
+        part_2_joltages.append(process_bank(12, bank_array))
 
-    print(sum(joltages))
+
+
+    print(part_1_joltages)
+
+    print(sum(part_1_joltages))
+    print(sum(part_2_joltages))
