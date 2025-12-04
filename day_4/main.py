ec0f996a3ba4bb02f7386cd9df78a6d15e550a1a
Francis Conroy <francisc@posteo.net>
2025-12-04 21:03:55 +1100

    Solution working

---
+++ day_4/main.py
@@ -0,0 +1,72 @@
+import pdb
+
+SUBGRID_SIZE = 3
+PADDING = 1
+def count_adjacents_in_subgrid(subgrid: list[list[str]]) -> int:
+    count = 0
+    for rowidx in range(SUBGRID_SIZE):
+        for cellidx in range(SUBGRID_SIZE):
+            if cellidx == 1 == rowidx:
+                continue  # skip center
+            try:
+                if subgrid[rowidx][cellidx] == '@':
+                    count += 1
+            except Exception:
+                pdb.pm()
+    return count
+
+def extend_grid(grid: list[list[str]]) -> list[list[str]]:
+    width = len(grid[0])
+    # extend lines
+    ogrid= []
+    for row in grid:
+        ogrid.append([""] + row + [""])
+
+    # Add top and bottom
+    ogrid.insert(0, [""] * (width + 2))
+    ogrid.append([""] * (width + 2))
+    return ogrid
+
+
+def main():
+    grid = []
+    with open('input.txt', "r") as readfile:
+        content = readfile.readlines()
+        for line in content:
+            line = line.strip('\n')
+            line = list(line)
+            grid.append(line)
+
+    grid = extend_grid(grid)
+
+    # iterate over grid excluding borders
+    grid_width = len(grid[0])
+    grid_height = len(grid)
+
+    total_count = 0
+    loop_removed_amount = -1
+    while loop_removed_amount != 0:
+        loop_removed_amount = 0
+        for rowidx in range(PADDING, grid_height - PADDING):
+            for cellidx in range(PADDING, grid_width - PADDING):
+                cell = grid[rowidx][cellidx]
+                if cell != '@':
+                    continue
+                # extract subgrid
+                subgrid = []
+                for rowoffset in range(-PADDING, PADDING+1):
+                    subrow = grid[rowidx+rowoffset][cellidx-PADDING:cellidx+PADDING+1]
+                    subgrid.append(subrow)
+                count = count_adjacents_in_subgrid(subgrid)
+                if count <4:
+                    loop_removed_amount +=1
+                    total_count += 1
+                    grid[rowidx][cellidx] = "x"
+    print(total_count)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ day_4/test_main.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from main import count_adjacents_in_subgrid
+
+
+class Test(TestCase):
+    def test_count_adjacents_in_subgrid(self):
+        self.assertEqual(2, count_adjacents_in_subgrid([["1","2","@"],
+                                                     ["4","5","6"],
+                                                     ["7","@","9"]]))
+
+        self.assertEqual(4, count_adjacents_in_subgrid([["","",""],
+                                                     ["@","@","@"],
+                                                     ["@","","@"]]))
